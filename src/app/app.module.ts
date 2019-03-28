7748ea8f5738ff8a961590f593f54a5512727539
jinsiko <lequangnghia1997@gmail.com>
2019-03-28 09:56:38 +0700

    bai tap ve service va route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {DictionaryComponent} from './dictionary/dictionary.component';
+import {DictionaryService} from './dictionary.service';
+import { DictionaryPageComponent } from './dictionary-page/dictionary-page.component';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    DictionaryComponent,
+    DictionaryPageComponent,
+
+],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DictionaryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/dictionary/dictionary.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {DictionaryService, IWord} from '../dictionary.service';
+
+@Component({
+  selector: 'app-dictionary',
+  templateUrl: './dictionary.component.html',
+  styleUrls: ['./dictionary.component.scss']
+})
+export class DictionaryComponent {
+  word: IWord;
+  constructor(private dictionaryService: DictionaryService) { }
+  search(word: string) {
+    const meaning = this.dictionaryService.search(word);
+    this.word = {
+      key: word,
+      meaning
+    };
+  }
+}
